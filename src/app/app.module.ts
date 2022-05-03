1503a7162cc230d3b9dc8f1af92adb6ac2bb2378
Juanco-23 <j.diego.db@gmail.com>
2022-05-02 19:16:48 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarraComponent } from './componente/barra/barra.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
